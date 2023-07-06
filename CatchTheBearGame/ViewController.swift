b8ed3a297ca972d3784ff570f978bdd52aefbc7f
Mehmet Güven <guvennmehmet37@gmail.com>
2023-07-06 16:15:49 +0300

    design

--- CatchTheBearGame/ViewController.swift
+++ CatchTheBearGame/ViewController.swift
@@ -8,7 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    
+    @IBOutlet weak var bear1: UIImageView!
+    @IBOutlet weak var bear2: UIImageView!
+    @IBOutlet weak var bear3: UIImageView!
+    @IBOutlet weak var bear4: UIImageView!
+    @IBOutlet weak var bear5: UIImageView!
+    @IBOutlet weak var bear6: UIImageView!
+    @IBOutlet weak var bear7: UIImageView!
+    @IBOutlet weak var bear8: UIImageView!
+    @IBOutlet weak var bear9: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
