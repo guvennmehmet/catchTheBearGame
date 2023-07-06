979153822449916410788959750410d44081f761
Mehmet Güven <guvennmehmet37@gmail.com>
2023-07-06 17:21:32 +0300

    replay function

--- CatchTheBearGame/ViewController.swift
+++ CatchTheBearGame/ViewController.swift
@@ -117,6 +117,14 @@ class ViewController: UIViewController {
             let replayButton = UIAlertAction(title: "Replay", style: UIAlertAction.Style.default) {
                 (UIAlertAction) in
                 //replay function
+                self.score = 0
+                self.scoreLabel.text = "Score: \(self.score)"
+                self.counter = 10
+                self.timeLabel.text = String(self.counter)
+                
+                self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.countDown), userInfo: nil, repeats: true)
+                self.hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.hideBear), userInfo: nil, repeats: true)
+                
             }
             
             alert.addAction(okButton)
